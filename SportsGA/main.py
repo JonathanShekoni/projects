cd6356406c079216186eb5da11ce39b56969dd85
Jonathan Shekoni <jonathanshekoni1@gmail.com>
2026-02-25 14:09:13 -0500

    api addition

--- SportsGA/main.py
+++ SportsGA/main.py
@@ -1,15 +1,29 @@
 from flask import Flask, redirect, url_for
+from nba_api.stats.endpoints import playercareerstats
 app = Flask(__name__) # Flask constructor
 
+#Nba api testing
+career = playercareerstats.PlayerCareerStats(player_id='203999')
+
+#pandas data frames
+career.season_totals_regular_season.get_data_frame()
+
+#json
+career.get_json
+
+#dictionary
+career.get_dict
+
+
+print(career.get_dict)
+
+#Flask testing
 #Decorator used to tell the application which URL is associated with the function
 @app.route('/')
 
 def home():
-    return "Sports Analytics API Running"
+    return f"Sports Analytics API Running: {career.get_json}"
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-#Added to Github
